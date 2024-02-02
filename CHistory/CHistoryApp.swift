a0b14f295f3eec60a5270cb3c8a0e830416228ea
suyashonkar <suyashonkar@gmail.com>
2024-02-02 15:48:44 +0530

    Initial Commit

---
+++ CHistory/CHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CHistoryApp.swift
+//  CHistory
+//
+//  Created by Suyash Onkar on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
